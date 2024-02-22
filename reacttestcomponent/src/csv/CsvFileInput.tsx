5e1ceb5a06ba620d21fdd401a85faaf957aabc61
Michael Javier <m.mota@concentra.com.do>
2024-02-22 10:02:31 -0400

    ejemplo con archivo csv

--- reacttestcomponent/src/csv/CsvFileInput.tsx
+++ reacttestcomponent/src/csv/CsvFileInput.tsx
@@ -5,11 +5,13 @@ export const CsvFileInput: React.FC = () => {
   const [csvData, setCsvData] = useState<any[]>([]);
 
   const handleFileUpload = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log('Michael ', e, e.target, e.target.files);
     const file = e.target.files?.[0];
     if (file) {
       Papa.parse(file, {
         header: true,
         complete: (result) => {
+          console.log('Termino ', result, result?.data);
           setCsvData(result.data);
         },
         error: (error) => {
